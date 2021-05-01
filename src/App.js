d995b25bb908be982e9132edefebaaae5dfe1f05
Inga Kozlowska <81585384+ingakoz@users.noreply.github.com>
2021-05-01 19:45:31 +0200

    logo deleted

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <App />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
